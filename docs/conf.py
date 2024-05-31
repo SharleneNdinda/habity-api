f8939a6b2c2fafa09b0658dbc0aeaeff0de2c6b8
Sharlene Ndinda <sharlenendinda@gmail.com>
2024-05-31 12:35:41 +0300

    feat: implement User Authentication using JWT

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
 import os
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -7,23 +8,22 @@ import os
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'habity'
-copyright = '2024, Sharlene Ndinda'
-author = 'Sharlene Ndinda'
-release = '1.0'
+project = "habity"
+copyright = "2024, Sharlene Ndinda"
+author = "Sharlene Ndinda"
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'nature'
-html_static_path = ['_static']
+html_theme = "nature"
+html_static_path = ["_static"]
